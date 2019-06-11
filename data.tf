65f89c9f6e357598e6204233a353aed1a5cdc4f9
Salvian Reynaldi <salvian@traveloka.com>
2019-06-11 11:50:58 +0700

    remove default filter as image tags are not transferred

--- data.tf
+++ data.tf
@@ -4,14 +4,6 @@ data "aws_ami" "latest_service_image" {
   most_recent      = true
 
   filter = [
-    {
-      name   = "tag:Service"
-      values = ["${var.service_name}"]
-    },
-    {
-      name   = "tag:ProductDomain"
-      values = ["${var.product_domain}"]
-    },
     "${var.image_filters}",
   ]
 }
