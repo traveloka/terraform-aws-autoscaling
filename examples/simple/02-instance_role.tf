168cc3f84007525c1f6b47131290d096d7549c47
Sergey <1583284+1583284@users.noreply.github.com>
2019-08-05 23:49:10 +0800

    Use "ref" instead of "version" to lock instance_profile module in the example
    Github source doesn't support version constraints:
    
    * terraform 0.11.14 ignores the version constraints and loads latest version
    
    > Initializing modules...
    > - module.instance_profile
    >   Getting source "github.com/traveloka/terraform-aws-iam-role.git//modules/instance"
    
    * terraform 0.12.6 fails with
    
    > Error: Invalid version constraint
    >
    > Cannot apply a version constraint to module "instance_profile" (at
    > 02-instance_role.tf:1) because it has a non Registry URL.

--- examples/simple/02-instance_role.tf
+++ examples/simple/02-instance_role.tf
@@ -1,6 +1,5 @@
 module "instance_profile" {
-  source  = "github.com/traveloka/terraform-aws-iam-role.git//modules/instance"
-  version = "v1.0.1"
+  source = "github.com/traveloka/terraform-aws-iam-role.git//modules/instance?ref=v1.0.1"
 
   service_name   = "fprbe"
   cluster_role   = "app"
