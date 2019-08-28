e954a4179a048e2594c132aaaaee863763a79eea
Agus Prasetiyo <aoktox@users.noreply.github.com>
2019-08-28 15:45:38 +0700

    terraform fmt

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "latest_service_image" {
-  owners           = ["${var.image_owners}"]
-  most_recent      = true
+  owners      = ["${var.image_owners}"]
+  most_recent = true
 
   filter = [
     "${var.image_filters}",
