e720af18c8541abcdf0d376ea1b6417d2ed37d70
Salvian Reynaldi <salvian@traveloka.com>
2020-04-23 14:01:53 +0700

    fix image_filters type

--- variables.tf
+++ variables.tf
@@ -176,7 +176,11 @@ variable "cpu_credits" {
 }
 
 variable "image_filters" {
-  type        = list(object(name=string,values=list(string)))
+  type = list(object({
+    name   = string,
+    values = list(string),
+    })
+  )
   description = "The AMI search filters. The most recent AMI that pass this filter will be deployed to the ASG. See https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-images.html"
 }
 
